985f61586c78380c2701adcb345400634571831b
root <root@test0502.sub09211432560.internet.oraclevcn.com>
2022-05-04 06:25:49 +0000

    feat(daily):commit daily job

---
+++ static/js/auth.js
@@ -0,0 +1,6 @@
+function queryParams() {
+    var username = localStorage.getItem("username");
+    // var token = localStorage.getItem("token");
+    return 'username=' + username;
+    // + '&token=' + token
+  }
